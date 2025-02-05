d182788e3e801d8924c1ed846fc4938e64cdde2c
Adrien Di Pasquale <adrien@dipasquale.fr>
2025-02-05 09:38:35 +0100

    remove block

--- lib/dsfr-form_builder.rb
+++ lib/dsfr-form_builder.rb
@@ -77,7 +77,7 @@ module Dsfr
       select(attribute, choices, { include_blank: opts[:include_blank] }, class: "fr-select")
     end
 
-    def dsfr_radio_buttons(attribute, choices, legend: nil, hint: nil, **opts, &block)
+    def dsfr_radio_buttons(attribute, choices, legend: nil, hint: nil, **opts)
       legend_content = @template.safe_join([
         legend || @object.class.human_attribute_name(attribute),
         hint ? hint_tag(hint) : nil
@@ -86,8 +86,7 @@ module Dsfr
         @template.safe_join(
           [
             @template.content_tag(:legend, legend_content, class: "fr-fieldset__legend--regular fr-fieldset__legend"),
-            choices.map { |c| dsfr_radio_option(attribute, value: c[:value], label_text: c[:label], hint: c[:hint], **opts) },
-            block_given? ? @template.content_tag(:div, class: "fr-fieldset__element", &block) : nil
+            choices.map { |c| dsfr_radio_option(attribute, value: c[:value], label_text: c[:label], hint: c[:hint], **opts) }
           ]
         )
       end
