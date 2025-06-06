83f8c2b2aa7b44d8797fe79a88b2eb02c5180cb7
Goulven Champenois <goulven.champenois@beta.gouv.fr>
2025-06-06 09:01:20 +0200

    fix: Improve check_box helper (#45)
    * refactor: Remove unnecessary whitespace
    * test: Ensure every dsfr_check_box config option is tested
    * feat: Add inline option to dsfr_check_box helper
    * fix: Prevent check_box helper options from ending up as attributes on the input

--- spec/dsfr-form_builder_spec.rb
+++ spec/dsfr-form_builder_spec.rb
@@ -98,13 +98,13 @@ RSpec.describe Dsfr::FormBuilder do
       HTML
     end
 
-    context 'with label and hint personalisation' do
+    context "with custom settings" do
       it 'generates the correct HTML' do
-        expect(builder.dsfr_check_box(:name, label: "Nom", hint: "Votre nom")).to match_html(<<~HTML)
-          <div class="fr-fieldset__element">
+        expect(builder.dsfr_check_box(:name, { label: "Nom", hint: "Votre nom", inline: true }, "checked", "unchecked")).to match_html(<<~HTML)
+          <div class="fr-fieldset__element fr-fieldset__element--inline">
             <div class="fr-checkbox-group">
-              <input name="record[name]" type="hidden" value="0" autocomplete="off">
-              <input label="Nom" hint="Votre nom" type="checkbox" value="1" name="record[name]" id="record_name" />
+              <input name="record[name]" type="hidden" value="unchecked" autocomplete="off">
+              <input type="checkbox" value="checked" name="record[name]" id="record_name" />
               <label class="fr-label" for="record_name">
                 Nom
                 <span class="fr-hint-text">
